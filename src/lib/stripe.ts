093e79d340a6ac12eba13ed72ce6ede560a03ece
Zdenek Ferenc <259133@vutbr.cz>
2025-12-27 18:22:55 +0100

    fix: oprava verze stripe

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -5,6 +5,5 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-11-17.clover',
-  typescript: true,
+  apiVersion: '2025-12-15.clover', 
 });
